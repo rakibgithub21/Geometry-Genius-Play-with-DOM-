6d1f962cc6099fd1a7b4a49960b50e973368ed8b
rakibgithub21 <mdrakiulislam.gff@gmail.com>
2024-02-13 08:33:37 +0600

    rectangle functionality done

--- scripts/rectangleArea.js
+++ scripts/rectangleArea.js
@@ -1,3 +1,14 @@
 function calculateRectangleArea() {
+    const rectangleLengthInput = document.getElementById('rectangle-length');
+    const rectangleLengthValue = rectangleLengthInput.value;
+    const length = parseFloat(rectangleLengthValue);
     
+    const rectangleWidthInput = document.getElementById('rectangle-width');
+    const rectangleWidthValue = rectangleWidthInput.value;
+    const width = parseFloat(rectangleWidthValue);
+
+    const area = length * width;
+
+    const rectangleArea = document.getElementById('rectangle-area');
+    rectangleArea.innerText = area;
 }
